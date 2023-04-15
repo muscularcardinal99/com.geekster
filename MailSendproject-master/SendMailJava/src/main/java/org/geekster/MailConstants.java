3ea3cc396349b84b68659b1105c250d9ed17a908
muscularcardinal99 <kushwaha.abhishek1999@gmail.com>
2023-04-15 16:03:42 +0530

    Added Mail sender Project using Api

---
+++ MailSendproject-master/SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,15 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "kushwaha.abhishek1999@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "mainakgh1@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 14-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is to send mail using API...!!!";
+}
---
+++ springAnnotation/src/main/java/com/geekster/springAnnotation/SpringAnnotationApplication.java
@@ -0,0 +1,15 @@
+package com.geekster.springAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(SpringAnnotationApplication.class, args);
+	}
+
+}
